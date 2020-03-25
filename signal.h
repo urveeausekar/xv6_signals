f619f0c86c40fb3cce3fc1f4806d43cd9e256610
auocoder <ausekaruo17.comp@coep.ac.in>
2020-03-25 22:55:57 +0530

    signal numbers defined

---
+++ signal.h
@@ -0,0 +1,21 @@
+//These are the signals defined in the original POSIX.1-1990 standard
+
+#define SIGHUP        1       //Term    Hangup detected on controlling terminal or death of controlling process
+#define SIGINT        2       //Term    Interrupt from keyboard
+#define SIGQUIT       3       //Core    Quit from keyboard
+#define SIGILL        4       //Core    Illegal Instruction
+#define SIGABRT       6      // Core    Abort signal from abort(3)
+#define SIGFPE        8      // Core    Floating-point exception
+#define SIGKILL       9      // Term    Kill signal
+#define SIGSEGV      11      // Core    Invalid memory reference
+#define SIGPIPE      13       //Term    Broken pipe: write to pipe with no readers; see pipe(7)
+#define SIGALRM      14       //Term    Timer signal from alarm(2)
+#define SIGTERM      15       //Term    Termination signal
+#define SIGUSR1      10   //Term    User-defined signal 1
+#define SIGUSR2      12    //Term    User-defined signal 2
+#define SIGCHLD      17    //Ign     Child stopped or terminated
+#define SIGCONT      18    //Cont    Continue if stopped
+#define SIGSTOP      19    //Stop    Stop process
+#define SIGTSTP      20    //Stop    Stop typed at terminal
+#define SIGTTIN      21    //Stop    Terminal input for background process
+#define SIGTTOU      22    //Stop    Terminal output for background process
