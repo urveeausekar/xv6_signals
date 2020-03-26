1770a92e088322de9ac231dcec40791ee62520f0
auocoder <ausekaruo17.comp@coep.ac.in>
2020-03-26 21:48:49 +0530

    data structures and syscalls definitions

--- signal.h
+++ signal.h
@@ -19,3 +19,36 @@
 #define SIGTSTP      20    //Stop    Stop typed at terminal
 #define SIGTTIN      21    //Stop    Terminal input for background process
 #define SIGTTOU      22    //Stop    Terminal output for background process
+
+#define SIG_DFL 0
+#define SIG_IGN 1
+#define SIG_USERDEF 2
+
+
+#define TERM 1
+#define IGN 2
+#define CORE 3
+#define CONT 4
+#define STOP 5
+
+int def_disposition[32] = {0, TERM, TERM, CORE, CORE, 0, CORE, 0, CORE, TERM, TERM, CORE, TERM, TERM, TERM, TERM, 0, IGN, CONT, STOP, STOP, STOP, STOP};
+
+#define NUMSIG 32 //maximum number of signals supported
+
+
+typedef void (*sighandler_t)(int);
+
+
+struct signinfo {
+  //int signum;		//number of the sent signal
+  int from_pid;		//signal sent by which process? Its pid
+  uint from_ecx;	//ecx of the signal that sent the signal
+};
+
+struct handlerinfo {
+	//int signum
+	int disposition;	//either SIG_DFL, SIG_IGN, or SIG_USERDEF
+	sighandler_t handler;	//address of the user defined signal handler
+};
+
+
